efa5f801f5bfb10fe5dd1c297eb2b61c87183bdb
XiaSibo <74701173+XiaSibo@users.noreply.github.com>
2021-04-07 18:03:18 +0800

    lab5

---
+++ Lab5_Implicit_Intents/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+    }
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        startActivity(intent);
+    }
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_text_with)
+                .setText(txt)
+                .startChooser();
+    }
+    public void takePic(View view) {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(takePicture);
+    }
+}
